e7dce9152d69fb57c6270dcba0ab16db8743a83f
Rubin <rubinprasadluitel@gmail.com>
2026-07-16 20:29:58 +0545

    edit space error in  superuser creation

--- backend/build.sh
+++ backend/build.sh
@@ -5,7 +5,7 @@ uv sync
 uv run python manage.py collectstatic --no-input
 uv run python manage.py migrate
 
-if [[$CREATE_SUPERUSER]];
+if [[ $CREATE_SUPERUSER ]];
 then
   uv run python manage.py createsuperuser --no-input
 fi
